3ec137694b6db7aecdb024091bbf4d3118c39ba7
OmelianIvanna <danit.vozmozno@gmail.com>
2026-04-10 16:56:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+const input = document.getElementById("task");
+
 function addTask() {
 
-const input = document.getElementById("task");
 const list = document.getElementById("list");
+if (input.value === "") return;
 const li = document.createElement("li");
-li.textContent = input.value;
+
+const checkbox = document.createElement("input");
+checkbox.type = "checkbox";
+checkbox.onchange = function() {
+li.style.textDecoration = checkbox.checked ? "line-through" : "none";
+};
+
+const span = document.createElement("span");
+span.textContent = input.value;
+li.appendChild(checkbox);
+li.appendChild(span);
 list.appendChild(li);
 input.value = "";
+}
 
-} 
+input.addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+        addTask();
+    }
+});
+
